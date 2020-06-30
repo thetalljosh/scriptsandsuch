e090bd4aef867a52cbd939c9c9038552162d8e14
thetalljosh <49563871+thetalljosh@users.noreply.github.com>
2020-06-30 11:33:42 -0400

    Update CatFacts.ps1

--- CatFacts.ps1
+++ CatFacts.ps1
@@ -1,8 +1,15 @@
-#Run this every 1/2 hour and in an 8 hour work day there will be approximately 3 times per day that your victim hears a cat fact
+#To prank your coworkers, remove the comment on lines 3 and 15 and run this every 1/2 hour on someone's pc. In an 8 hour work day this will produce roughly 3 cat facts
+
 if ((Get-Random -Maximum 10000) -lt 1875) {
-    Add-Type -AssemblyName System.Speech
-    $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-    $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'http://catfacts-api.appspot.com/api/facts')).facts
-    $SpeechSynth.Speak("did you know?")
-    $SpeechSynth.Speak($CatFact)
-} 
+
+Add-Type -AssemblyName System.Speech
+$SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+class fact {
+    [string] $fact
+    [int] $length
+    [string] ToString() { return $this.fact }
+}
+$SpeechSynth.Speak("Did you know? $([fact](irm -useb https://catfact.ninja/fact))")
+
+}
