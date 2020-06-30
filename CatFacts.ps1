d3256a59f0c82b94054a282b1ead1709ef974f99
thetalljosh <49563871+thetalljosh@users.noreply.github.com>
2020-06-30 11:34:21 -0400

    Update CatFacts.ps1

--- CatFacts.ps1
+++ CatFacts.ps1
@@ -1,6 +1,6 @@
 #To prank your coworkers, remove the comment on lines 3 and 15 and run this every 1/2 hour on someone's pc. In an 8 hour work day this will produce roughly 3 cat facts
 
-if ((Get-Random -Maximum 10000) -lt 1875) {
+#if ((Get-Random -Maximum 10000) -lt 1875) {
 
 Add-Type -AssemblyName System.Speech
 $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
@@ -12,4 +12,4 @@ class fact {
 }
 $SpeechSynth.Speak("Did you know? $([fact](irm -useb https://catfact.ninja/fact))")
 
-}
+#}
