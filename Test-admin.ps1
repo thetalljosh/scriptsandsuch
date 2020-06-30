e9be13dceaabc99696fed196706c2fdeb7f4f4a6
thetalljosh <49563871+thetalljosh@users.noreply.github.com>
2020-06-30 10:35:35 -0400

    Create Test-admin.ps1

---
+++ Test-admin.ps1
@@ -0,0 +1,23 @@
+#Add this to the top of any powershell script to force it to run with elevated credentials
+#this param function block forces the script to run under elevated token
+
+param([switch]$Elevated)
+
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) 
+    {
+        # tried to elevate, did not work, aborting
+    } 
+    else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+}
+
+exit
+}
+
+'running with full privileges'
